22af8833fae43d778d97c18425ab5e84c19902fd
Lúthien <luthien@parendili.org>
2023-07-04 15:07:56 +0200

    added static openApiDocs and static Swagger UI deployment

--- public/console/swagger-initializer.js
+++ public/console/swagger-initializer.js
@@ -3,8 +3,8 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    // url: "https://petstore.swagger.io/v2/swagger.json",
-    url: "../api/api-docs",
+    //url: "../api/api-docs",
+    queryConfigEnabled: true,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
